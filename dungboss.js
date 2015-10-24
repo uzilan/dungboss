8994f7f24afa5ca11955e05a7a315ccab2f3a51c
Uzi Landsmann <uzi.landsmann@gmail.com>
2015-10-24 23:20:52 +0200

    fixed images and some more filtering

--- dungboss.js
+++ dungboss.js
@@ -7,28 +7,61 @@ dungboss.controller('DungbossController', ['$scope', '$http',
             $scope.heroes = data;
 
             $scope.elements = uniques("element");
-            $scope.classes = uniques("class");
+            $scope.classes = extractClasses();
+
+            $scope.selectAllElements = true;
+            $scope.selectAll('elements');
+
+            $scope.selectAllClasses = true;
+            $scope.selectAll('classes');
+
         });
 
         var uniques = function (type) {
             return _.chain($scope.heroes)
                 .map(type)
                 .uniq()
+                .sort()
                 .map(function (name) {
                     return {name: name, enabled: false};
                 })
+                .value();
+        };
+
+        var extractClasses = function () {
+            return _.chain($scope.heroes)
+                .reduce(function (result, h) {
+                    return result.concat(_.words(h.class));
+                }, [])
+                .uniq()
                 .sort()
+                .map(function (clazz) {
+                    return {name: clazz, enabled: false}
+                })
                 .value();
         };
 
         var isEnabled = function (hero) {
             var elementIndex = findIndex($scope.elements, hero.element);
+            if (elementIndex === -1) {
+                return false;
+            }
             var element = $scope.elements[elementIndex];
+            if (!element.enabled) {
+                return false;
+            }
 
-            var classIndex = findIndex($scope.classes, hero.class);
-            var clazz = $scope.classes[classIndex];
-
-            return element.enabled && clazz.enabled;
+            var result = false;
+            _.each(_.words(hero.class), function (c) {
+                var classIndex = findIndex($scope.classes, c);
+                if (classIndex > -1) {
+                    var clazz = $scope.classes[classIndex];
+                    if (clazz.enabled) {
+                        result = true;
+                    }
+                }
+            });
+            return result;
         };
 
         $scope.selectHeroes = function () {
@@ -36,13 +69,15 @@ dungboss.controller('DungbossController', ['$scope', '$http',
                 .filter(function (hero) {
                     return isEnabled(hero);
                 })
-                .sort()
+                .sortBy(function (hero) {
+                    return hero.name;
+                })
                 .value();
         };
 
         var findIndex = function (items, value) {
             return _.findIndex(items, function (v) {
-                return v.name === value;
+                return value.indexOf(v.name) > -1;
             });
         };
 
@@ -56,6 +91,10 @@ dungboss.controller('DungbossController', ['$scope', '$http',
                     sa = $scope.selectAllClasses;
                     items = $scope.classes;
                     break;
+                case 'elements':
+                    sa = $scope.selectAllElements;
+                    items = $scope.elements;
+                    break;
             }
 
             if (sa) {
@@ -65,7 +104,7 @@ dungboss.controller('DungbossController', ['$scope', '$http',
             }
 
             angular.forEach(items, function (item) {
-                var i = findIndex($scope.classes, item.name);
+                var i = findIndex(items, item.name);
                 items[i].enabled = $scope.selectedAll;
             });
             $scope.selectHeroes();
